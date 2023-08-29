2a977d1484793c996c47c3622220129b6449dffd
Evan Joseph <evandromariano49@gmail.com>
2023-08-29 15:06:27 -0300

    3

---
+++ aula 01/src/components/TituloDinamico.js
@@ -0,0 +1,13 @@
+class TituloDinamico extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    //base do componete
+
+    //estilização do componente
+
+    //enviar para a shadow
+  }
+}
