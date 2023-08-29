95d44ce1a6b61ffc3bf424e5bbc03a09963ff8b1
Evan Joseph <evandromariano49@gmail.com>
2023-08-29 20:22:53 -0300

    7 - finalizado

--- aula 01/src/components/CardNews.js
+++ aula 01/src/components/CardNews.js
@@ -1,10 +1,9 @@
 class CardNews extends HTMLElement{
     constructor(){
         super();
-
         const shadow = this.attachShadow({mode:"open"});
         shadow.appendChild(this.build());
-        // shadow.appendChild(this.styles());
+        shadow.appendChild(this.styles());
     }
 
     build() {
@@ -22,6 +21,8 @@ class CardNews extends HTMLElement{
         
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
 
@@ -30,6 +31,9 @@ class CardNews extends HTMLElement{
         cardLeft.appendChild(newsContent);
 
         const newsImage = document.createElement("img");
+        // newsImage.src = "/assets/dart.jpeg" 
+        newsImage.src = this.getAttribute("img-src") || "/assets/default-photo.webp";
+        newsImage.alt = "imagem do Darth Vader"
 
         cardRight.appendChild(newsImage);
 
@@ -39,7 +43,50 @@ class CardNews extends HTMLElement{
         return componentRoot;
     }
 
-    styles() {}
+    styles() {
+        const style = document.createElement("style");
+
+        style.textContent = `
+        .card {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            /* justify-content: space-between; */
+          }
+          
+          .card_left,
+          .card_right {
+            /* border: 1px solid blue; */
+            box-shadow: 10px 10px 76px -10px rgba(0, 0, 0, 0.75);
+            -webkit-box-shadow: 10px 10px 76px -10px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: 10px 10px 76px -10px rgba(0, 0, 0, 0.75);
+          }
+          
+          .card_left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+          }
+          
+          .card_left > span {
+            font-weight: 400;
+          }
+          
+          .card_left > a  {
+            margin-top: 15px;
+            font-size: 25px;
+            color: blueviolet;
+            text-decoration: none;
+            font-weight: bold;
+          }
+          
+          .card_left > p {
+            color: gray;
+          }
+        `
+
+        return style;
+    }
 }
 
 
