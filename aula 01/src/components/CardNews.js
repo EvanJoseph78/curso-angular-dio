2440ac9e8f984b96998048c73678367d64ddb552
Evan Joseph <evandromariano49@gmail.com>
2023-08-29 19:04:28 -0300

    6 - adicionando atributos aos componetes

--- aula 01/src/components/CardNews.js
+++ aula 01/src/components/CardNews.js
@@ -17,6 +17,22 @@ class CardNews extends HTMLElement{
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card_right");
 
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+        
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const newsImage = document.createElement("img");
+
+        cardRight.appendChild(newsImage);
+
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
 
